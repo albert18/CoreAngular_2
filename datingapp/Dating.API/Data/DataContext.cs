302122f383f2aa3e60f8c6dfb2691f0ee31f5ea3
albert18 <caps18@hotmail.com>
2019-11-16 10:25:56 -0500

    Installed SQL server

--- datingapp/Dating.API/Data/DataContext.cs
+++ datingapp/Dating.API/Data/DataContext.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dating.API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dating.API.Data
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options): base(options) 
+        {
+            
+        }
         
+        public DbSet<Value> Values { get; set; }
     }
 }
