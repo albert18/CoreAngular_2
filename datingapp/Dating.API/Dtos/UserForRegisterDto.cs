ba3d254602554401eba13c2870071b4cda37a758
albert18 <caps18@hotmail.com>
2019-11-18 18:30:46 -0500

    update model

--- datingapp/Dating.API/Dtos/UserForRegisterDto.cs
+++ datingapp/Dating.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Dating.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify must password with more character.")]
         public string Password { get; set; }
     }
 }
